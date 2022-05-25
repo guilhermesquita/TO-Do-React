ea279108709c79d28ca5594c7e9e93563a480975
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-05-25 11:54:46 -0400

    manipulando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import {useState} from 'react'
 import * as C from './App.styles'
+import { Item } from './types/Item'
+import { ListItem } from './components/listItem'
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'compra pão', done: false},
+    {id: 2, name: 'Lavar o carro', done: true}
+  ])
+
   return (
     <C.Container>
-      <C.Area>...</C.Area>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+
+        {
+        
+        list.map((item, index) => (
+          <ListItem key={index} item={item}/>
+        ))
+        
+        }
+
+      </C.Area>
     </C.Container>
 );
 }
